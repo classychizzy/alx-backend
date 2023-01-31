6fb62bf4a29803b4b51febf0113b827317ea872a
classychizzy <chizobachukwuoma@gmail.com>
2023-01-31 21:23:59 +0000

    bug fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-"""
-a flask app that returns hello world
-"""
-
-
-from flask import Flask
-from flask import render_template
+""" 0. Basic Flask app """
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> str:
-    """ a function that returns a template"""
-    return render_template('0-index.html', title='Home')
+def index():
+    """ index """
+    return render_template("0-index.html")
 
 
 if __name__ == '__main__':
